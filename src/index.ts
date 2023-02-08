d7eed5acb31340525db913dc92cbab6248101377
jawaka345 <lu.k@p2p.legal>
2023-02-08 19:51:41 +0100

    remove Modification and HistBoard -> moved to gracofeu server

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ export * from './area';
 export * from './graph';
 export * from "./text_zone";
 export * from "./board";
-export * from "./hist_board";
-export * from "./board_modification";
 
 export * from "./representations/representation";
 export * from "./representations/degree_width_rep";
