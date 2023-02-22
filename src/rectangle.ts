5515598343495b2ed390da521d23438a24b2bca4
YlN <thiebaut.jocelyn@gmail.com>
2023-02-22 18:09:53 +0100

    rectangle.ts

---
+++ src/rectangle.ts
@@ -0,0 +1,29 @@
+import { Coord } from "./coord";
+
+export class Rectangle {
+    c1 : Coord;
+    c2 : Coord;
+
+    constructor(c1: Coord, c2: Coord){
+        this.c1 = c1.copy();
+        this.c2 = c2.copy();
+    }
+
+
+    top_right_corner(): Coord{
+        return new Coord(Math.max(this.c1.x, this.c2.x), Math.min(this.c1.y, this.c2.y))
+    }
+    
+    bot_left_corner(): Coord{
+        return new Coord(Math.min(this.c1.x, this.c2.x), Math.max(this.c1.y, this.c2.y))
+    }
+
+
+    top_left_corner(): Coord{
+        return new Coord(Math.min(this.c1.x, this.c2.x), Math.min(this.c1.y, this.c2.y))
+    }
+
+    bot_right_corner(): Coord{
+        return new Coord(Math.max(this.c1.x, this.c2.x), Math.max(this.c1.y, this.c2.y))
+    }
+}
